a1533395e9ff7d41d6dc69b6fe66c676d9c8ff49
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-24 20:31:37 +0300

    Addert Chart Solution on both variants

---
+++ AAP/homework/Solutions/Chart/Program.cs
@@ -0,0 +1,26 @@
+﻿// NET 6.0; C# 10; ImplicitUsings enabled
+
+double x = Console.ReadLine()!.AsDouble();
+double result = SecondVariant(x);
+Console.WriteLine(result);
+
+double FirstVariant(double x)
+{
+    const int period = 2;
+    const int amplitude = 1;
+    const int bias = 2;
+
+    double xMappedOnPeriod = Math.Abs(x) % period;
+
+    return xMappedOnPeriod <= amplitude
+        ? xMappedOnPeriod
+        : bias - xMappedOnPeriod;
+}
+
+double SecondVariant(double x) => FirstVariant(x) - 1;
+
+internal static class Extensions
+{
+    public static double AsDouble(this string s) => double.Parse(s);
+}
+
