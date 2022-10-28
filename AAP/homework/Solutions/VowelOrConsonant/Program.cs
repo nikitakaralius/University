185b4821b5ea316b532b1b593828066bf1fc1aab
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-28 17:44:48 +0300

    Added VowelOrConsonant solution

---
+++ AAP/homework/Solutions/VowelOrConsonant/Program.cs
@@ -0,0 +1,15 @@
+﻿char symbol = char.Parse(Console.ReadLine()!.ToLower());
+
+if (symbol < 'а' || symbol > 'я')
+{
+    Console.WriteLine("Error");
+    return;
+}
+
+bool isVowel = symbol switch
+{
+    'а' or 'у' or 'о' or 'и' or 'э' or 'ы' or 'я' or 'ю' or 'е' or 'ё' => true,
+    _                                                                  => false
+};
+
+Console.WriteLine(isVowel ? "Гласная" : "Согласная");
