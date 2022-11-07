65b96d644b3cb036e4e99f0866dcc43b9a0622e5
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-07 16:49:38 +0300

    Added Largest Student Group Solution

---
+++ AAP/homework/Solutions/LargestStudentGroup/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int currentAge, previousAge = 0;
+        int sequenceLength = 0, longestSequenceLength = 0;
+
+        do
+        {
+            currentAge = int.Parse(Console.ReadLine()!);
+
+            if (currentAge != previousAge)
+                sequenceLength = 0;
+
+            sequenceLength++;
+            previousAge = currentAge;
+
+            longestSequenceLength = Math.Max(sequenceLength, longestSequenceLength);
+        }
+        while (currentAge != 0);
+
+        Console.WriteLine(longestSequenceLength);
+    }
+}
