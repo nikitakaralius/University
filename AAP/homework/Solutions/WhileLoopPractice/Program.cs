0f64ca130e320ddd4bef8ed871a65b27447239f9
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-12 12:48:26 +0300

    Added While Loop Practice solution

---
+++ AAP/homework/Solutions/WhileLoopPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const double timeStep = 20.0 / 60.0;
+
+        double volume = double.Parse(Console.ReadLine()!);
+        double sailorSpeed = double.Parse(Console.ReadLine()!);
+        double waterSpeed = double.Parse(Console.ReadLine()!);
+
+        double sailorWorkIncrement = int.Parse(Console.ReadLine()!) / 100.0;
+        double waterWorkIncrement = int.Parse(Console.ReadLine()!) / 100.0;
+
+        double requiredTime = 0;
+
+        while (volume > 0)
+        {
+            volume -= sailorSpeed * timeStep;
+            volume += waterSpeed * timeStep;
+
+            sailorSpeed += sailorSpeed * sailorWorkIncrement;
+            waterSpeed += waterSpeed * waterWorkIncrement;
+
+            requiredTime += timeStep;
+        }
+
+        Console.WriteLine((int) requiredTime);
+        Console.ReadKey();
+    }
+}
