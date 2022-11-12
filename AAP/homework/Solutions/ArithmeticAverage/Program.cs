cfa6f1b4a8da0b60488de729c1dac7e3354076da
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-12 13:00:18 +0300

    Added Arithmetic Average solution

---
+++ AAP/homework/Solutions/ArithmeticAverage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int value, count = 0;
+        double sum = 0;
+
+        do
+        {
+            value = int.Parse(Console.ReadLine()!);
+            sum += value;
+            count++;
+        }
+        while (value != 0);
+
+        Console.WriteLine(sum / (count - 1));
+        Console.ReadKey();
+    }
+}
