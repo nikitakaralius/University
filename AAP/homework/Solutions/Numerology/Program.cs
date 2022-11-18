67e11dd9d7966bf45e47533801ba54c34ef0fdc7
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-18 16:53:15 +0300

    Added Numerology Solution

---
+++ AAP/homework/Solutions/Numerology/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int date = int.Parse(Console.ReadLine()!);
+        int answer = Numerology(date);
+        Console.WriteLine(answer);
+        Console.ReadKey();
+    }
+    private static int Numerology(int date)
+    {
+        int digitsSum = DigitsSum(date);
+
+        while (digitsSum / 10 > 0)
+            digitsSum = DigitsSum(digitsSum);
+
+        return digitsSum;
+    }
+
+    private static int DigitsSum(int value)
+    {
+        int sum = 0;
+        while (value > 0)
+        {
+            sum += value % 10;
+            value /= 10;
+        }
+
+        return sum;
+    }
+}
