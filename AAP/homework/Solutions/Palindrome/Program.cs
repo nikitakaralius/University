15bdd08e83b9089e76c9348b8388588fde11d25b
nikitakaralyus <nikitakaralyus@gmail.com>
2022-12-03 17:22:14 +0300

    Created Palindrome solution

---
+++ AAP/homework/Solutions/Palindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите слово или фразу, которые хотите проверить на палиндром");
+        string s = Console.ReadLine()!.ToLower().Replace(" ", "");
+        Console.WriteLine(IsPalindrome(s) ? "палиднром ✅" : "не палиндром ❌");
+        Console.ReadKey();
+    }
+
+    private static bool IsPalindrome(string s)
+    {
+        if (s.Length <= 1) return true;
+        if (s[0] != s[^1]) return false;
+        return IsPalindrome(s[1..^1]);
+    }
+}
