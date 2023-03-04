c4419dd66590a3eba3e170514beabe5d98ed0410
nikitakaralius <nikitakaralyus@gmail.com>
2023-03-04 16:07:44 +0300

    Added solutions

---
+++ AAP/homework/Solutions/JaggedArraySort/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        RunFirstTestcase();
+        RunSecondTestcase();
+    }
+
+    private static void RunFirstTestcase()
+    {
+        int[][] arrTaxi = new int[10][];
+
+        arrTaxi[0] = new int[] {100, 289, 200, 101, 90, 230};
+        arrTaxi[1] = new int[] {290, 300, 303, 120, 150};
+        arrTaxi[2] = new int[] {80};
+        arrTaxi[3] = new int[] {300, 60, 120, 400, 410};
+        arrTaxi[4] = new int[] {60, 100, 40};
+        arrTaxi[5] = new int[] {60, 160, 165, 120, 110, 230, 200, 30};
+        arrTaxi[6] = new int[] {230, 200, 250, 100};
+        arrTaxi[7] = new int[] {100, 209, 175, 100};
+        arrTaxi[8] = new int[] {70, 120, 290};
+        arrTaxi[9] = new int[] {90, 80, 105, 140, 120};
+
+        RunTestcase(arrTaxi);
+    }
+
+    private static void RunSecondTestcase()
+    {
+        int[][] arrTaxi = new int[10][];
+
+        arrTaxi[0] = new int[] {80};
+        arrTaxi[1] = new int[] {60, 100, 40};
+        arrTaxi[2] = new int[] {70, 120, 290};
+        arrTaxi[3] = new int[] {100, 209, 175, 100};
+        arrTaxi[4] = new int[] {230, 200, 250, 100};
+        arrTaxi[5] = new int[] {90, 80, 105, 140, 120};
+        arrTaxi[6] = new int[] {290, 300, 303, 120, 150};
+        arrTaxi[7] = new int[] {300, 60, 120, 400, 410};
+        arrTaxi[8] = new int[] {100, 289, 200, 101, 90, 230};
+        arrTaxi[9] = new int[] {60, 160, 165, 120, 110, 230, 200, 30};
+
+        RunTestcase(arrTaxi);
+    }
+
+    private static void RunTestcase(int[][] taxi)
+    {
+        int iterations = taxi.QuickSort((x, y) =>
+        {
+            if (x.Length == y.Length)
+                return x.Sum() - y.Sum();
+
+            return y.Length - x.Length;
+        });
+
+        Print(taxi, iterations);
+    }
+
+    private static void Print(int[][] jaggedArray, int iterations)
+    {
+        foreach (int[] array in jaggedArray)
+        {
+            foreach (int x in array)
+            {
+                Console.Write($"{x} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine($"Количество итераций = {iterations}");
+        Console.WriteLine();
+    }
+}
+
+internal static class ArrayExtensions
+{
+    public static int QuickSort<T>(this T[] array, Func<T, T, int> comparer) =>
+        QuickSort(array, 0, array.Length - 1, comparer);
+
+    public static int Sum(this int[] array)
+    {
+        int sum = 0;
+
+        foreach (int element in array)
+        {
+            sum += element;
+        }
+
+        return sum;
+    }
+
+    private static int QuickSort<T>(T[] array, int begin, int end, Func<T, T, int> comparer)
+    {
+        if (begin >= end) return 1;
+
+        int partitionIndex = EvaluatePartitionIndex(array, begin, end, comparer);
+
+        return 1 +
+               QuickSort(array, begin, partitionIndex - 1, comparer) +
+               QuickSort(array, partitionIndex + 1, end, comparer);
+    }
+
+    private static int EvaluatePartitionIndex<T>(T[] array, int begin, int end, Func<T, T, int> comparer)
+    {
+        T pivot = array[end];
+        int i = begin - 1;
+
+        for (int j = begin; j < end; j++)
+        {
+            if (comparer(array[j], pivot) <= 0)
+            {
+                i++;
+                Swap(ref array[i], ref array[j]);
+            }
+        }
+
+        Swap(ref array[i + 1], ref array[end]);
+
+        return i + 1;
+    }
+
+    private static void Swap<T>(ref T x, ref T y)
+    {
+        T temp = x;
+        x = y;
+        y = temp;
+    }
+}
---
+++ AAP/homework/Solutions/StructureDefenitions/Program.cs
@@ -0,0 +1,90 @@
+﻿Console.WriteLine();
+
+// Загородная недвижимость
+public struct SuburbanRealEstate
+{
+    public decimal Cost;     // Стоимость
+    public double Area;      // Площадь
+    public DateTime BuildAt; // Когда был построен
+    public Person Owner;     // Владелец
+    public Address Address;  // Адрес
+}
+
+// Акт нарушения пожарной безопасности
+public struct FireSafetyViolationAct
+{
+    public Address CompilationPlace; // Место составления акта
+    public DateTime CompiledAt;      // Дата составления
+    public Employee[] Inspectors;    // Проверяющие
+    public Violation[] Violations;   // нарушения
+    public Signature[] Signatures;   // Подписи
+}
+
+// Музыкальный коллектив
+public struct MusicBand
+{
+    public string Name;                // Название
+    public Musician[] Musicians;       // Музыканты
+    public DateTime FormedAt;          // Когда образована
+    public Composition[] Compositions; // Композиции
+}
+
+// Адрес
+public struct Address
+{
+    public string Settlement;  // Поселение
+    public string Street;      // Улица
+    public string HouseNumber; // Номер дома
+    public int Index;          // Индекс
+}
+
+// Человек
+public struct Person
+{
+    public string FirstName; // Имя
+    public string LastName;  //  Фамилия
+    public string Gender;    // Гендер
+    public int Age;          // Возраст
+}
+
+// Работник
+public struct Employee
+{
+    public Person Person;   // Данные о человеке
+    public string Position; // Должность
+}
+
+
+// Нарушение
+public struct Violation
+{
+    public string Title;           // Наименование нарушения
+    public string Description;     // Описание
+    public int Count;              // Количество нарушений
+    public string InspectorRemark; // Примечание составителя настоящего Акта
+    public string IntruderRemark;  // Примечание нарушителя
+}
+
+// Подпись
+public struct Signature
+{
+    public string Text;       // Текст подписи
+    public DateTime SignedAt; // Дата подписания
+}
+
+// Музыкант
+public struct Musician
+{
+    public Person Person;     // Данные о человеке
+    public string Instrument; // Музыкальный инструмент
+}
+
+// Композиция
+public struct Composition
+{
+    public string Name;          // Название
+    public string Album;         // Альбом
+    public DateTime ReleaseDate; // Дата выхода
+    public int PlaysCount;       // Количество воспроизведений
+    public int SecondsLength;    // Длительность в секундах
+}
